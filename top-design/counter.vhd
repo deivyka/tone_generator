9a51e4c8d081bafd6fdc8894c39d62af3c5bb8ed
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2019-09-12 20:54:28 +0200

    Update counter.vhd

--- top-design/counter.vhd
+++ top-design/counter.vhd
@@ -1,12 +1,11 @@
--- universal binary counter
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.std_logic_unsigned.all;
 use ieee.numeric_std.all;
 
 entity UniBitCnt is
-  Port (clk, clear: in std_logic;
+  Port (clk, reset: in std_logic;
+        clear_sig : in std_logic;
         c_out : out std_logic_vector(17 downto 0));
 end UniBitCnt;
 
@@ -14,17 +13,17 @@ architecture Behavioral of UniBitCnt is
     signal ffin, ffout : unsigned(17 downto 0);
 begin
 -- state register section
-process (clk)
+process (clk, reset)
 begin
-    if rising_edge(clk) then
+    if (reset='1') then
+        ffout <= (others => '0');
+    elsif rising_edge(clk) then
         ffout <= ffin;
     end if;
 end process;
-
 -- Next-state logic (combinational)
-ffin <= (others => '0') when (clear = '1') else -- clear c_out
+ffin <= (others => '0') when (clear_sig = '1') else -- clear c_out
         ffout+1;      -- count up
-
 -- output logic (combinational)
 c_out <= std_logic_vector(ffout);
 end Behavioral;
