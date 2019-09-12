4dad653d69afa0e5c1806c90e6c1798e0be76269
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2019-09-12 20:52:14 +0200

    Update top.vhd

--- top-design/top.vhd
+++ top-design/top.vhd
@@ -1,26 +1,24 @@
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity top is
-  Port (clk: in std_logic;
-        Do_btn, Re_btn, Mi_btn, Fa_btn, Sol_btn, La_btn, Ti_btn : in std_logic;  
+  Port (clk, reset: in std_logic;
+        play_notes : in std_logic_vector(2 downto 0);  
         audio_out : out std_logic);
 end top;
 
 architecture Behavioral of top is
+signal val : std_logic_vector(17 downto 0);
+signal clearing : std_logic;
+
 begin
+u1 : entity work.UniBitCnt port map(
+            clk => clk, reset => reset, clear_sig => clearing, c_out => val         
+);
 
 ut : entity work.tone_gen port map(
-        clk => clk,
-        Do => Do_btn,
-        Re => Re_btn,
-        Mi => Mi_btn,
-        Fa => Fa_btn,
-        Sol => Sol_btn,
-        La => La_btn,
-        Ti => Ti_btn,
-        audio_out => audio_out       
+        clk=>clk, val_in=>val, play_notes=>play_notes,
+        clearing=>clearing, audio_out=>audio_out       
 );
 
 end Behavioral;
